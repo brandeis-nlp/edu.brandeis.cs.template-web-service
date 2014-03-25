03e6b7819c3aac7d1960c10cec92788f97d60ed4
chunqishi <shicq@cs.brandeis.edu>
2014-03-25 17:29:28 -0400

    template web service for nltk

---
+++ src/main/java/edu/brandeis/cs/python/utils/Test.java
@@ -0,0 +1,17 @@
+package edu.brandeis.cs.python.utils;
+import java.io.IOException;
+
+import net.razorvine.pyro.*;
+
+public class Test {
+	public static void main(String[]args) throws IOException {
+		
+		NameServerProxy ns = NameServerProxy.locateNS(null);
+		PyroProxy remoteobject = new PyroProxy(ns.lookup("Your.Pyro.Object"));
+		Object result = remoteobject.call("pythonmethod", 42, "hello", new int[]{1,2,3});
+		String message = (String)result;  // cast to the type that 'pythonmethod' returns
+		System.out.println("result message="+message);
+		remoteobject.close();
+		ns.close();
+	}
+}
